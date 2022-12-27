8e908b53c8638e1d9228fce9349d8846bc4ba5d8
Melanie Tolvett S <melanietolvett@gmail.com>
2022-12-27 10:50:05 -0300

    intento router mix

--- src/main.js
+++ src/main.js
@@ -4,16 +4,85 @@ import { myFunction } from './lib/index.js';
 
 myFunction();
 
-const productsTemplate = 'this products';
-const cartTemplate = 'this cart';
-const checkoutTemplate = 'this checkout';
+const inicioTemplate = `
+<!-- PANTALLA INICIO -->
+  <section class="pantalla" id="inicio">
+    <section class="presentacion" id="presentacion">
+      <div class="logo" id=""></div>
+      <p class="frase" id="frase">Un mundo para tu mascota</p>
+    </section>
+
+    <form class="login" id="login">
+      <label for="loginEmail">*Ingrese un correo existente.</label>
+      <input type="email" class="datosLogin" id="loginEmail" placeholder="Correo*" value="">
+      <label for="loginPassword">*Ingrese una contraseña mayor a 5 dígitos.</label>
+      <input type="password" class="datosLogin" id="loginPassword" placeholder="Contraseña*" value="">
+      <button type="submit" class="loginBtn" id="loginBtn">Iniciar sesión</button>
+    </form>
+    <div class="loginGoogle"> Ingresa con Google
+
+    </div>
+    <section class="invitacion" id="invitacion">
+      <p class="pregunta" id="preguntas">¿No tienes una cuenta?</p>
+      <div class="" id="signUpOption">Regístrate</div>
+    </section>
+  </section>
+`;
+const registroTemplate = `
+<!-- PANTALLA REGISTRO -->
+  <section class="pantalla" id="registro">
+    <section class="presentacion" id="">
+      <div class="logo" id=""></div>
+      <p class="frase" id="frase">Un mundo para tu mascota</p>
+    </section>
+
+    <form class="registro" id="registro">
+      <label for="signUpName">*Ingrese el nombre de su mascota.</label>
+      <input type="text" class="datosRegistro" id="signUpName" placeholder="Nombre*" value="">
+      <label for="signUpEmail">*Ingrese un correo existente.</label>
+      <input type="email" class="datosRegistro" id="signUpEmail" placeholder="Correo*" value="">
+      <label for="signUpPassword">*Ingrese una contraseña mayor a 5 dígitos.</label>
+      <input type="password" class="datosRegistro" id="signUpPassword" placeholder="Contraseña*" value="">
+      <button type="submit" class="signUpBtn" id="signUpBtn">Crear cuenta</button>
+    </form>
+    <section class="invitacion" id="invitacion">
+      <p class="pregunta" id="preguntas">¿Ya tienes una cuenta?</p>
+      <div class="" id="signInOption">Inicia Sesión</div>
+    </section>
+  </section>
+`;
+const bienvenidaTemplate = `
+<!-- PANTALLA BIENVENIDA -->
+  <section class="pantalla" id="bienvenida">
+    <section class="presentacion" id="">
+      <p class="frase" id="">¡Hola NOMBRE, tu cuenta ha sido creada con exito!</p>
+      <div class="logo" id=""></div>
+      <p class="frase" id="">Recuerda que...</p>
+      <p class="frase" id="">Un mundo mejor es aquel donde
+        todos los animales tienen amigos
+        con los que compartir sus
+        aventuras.</p>
+    </section>
+
+    <section class="comenzar" id="">
+      <p class="frase" id="frase">¡Lista para ser parte de Petsbook!</p>
+      <button>Comenzar</button>
+    </section>
+  </section>
+`;
+
+const loginBtn = document.getElementById("loginBtn");
+const signUpOption = document.getElementById("signUpOption");
+const signInOption = document.getElementById("signInOption");
+const signUpBtn = document.getElementById("signUpBtn");
 
 //rutas y contenido de cada una
 const routes = {
-  '/': 'home',
-  '/products': productsTemplate,
-  '/cart': cartTemplate,
-  '/checkout': checkoutTemplate
+  '/': inicioTemplate,
+  '/registro': registroTemplate,
+  '/bienvenida': bienvenidaTemplate,
+  '/home': "Home",
+
 };
 
 //obtener la vista según la ruta actual
@@ -32,6 +101,24 @@ function showCurrentView() {
 window.addEventListener('popstate', showCurrentView);
 showCurrentView();
 
+loginBtn.addEventListener("submit", e => {
+  e.preventDefault();
+  history.pushState(null, "Home", "/home");
+});
+signUpOption.addEventListener("click", e => {
+  history.pushState(null, "Registro", "/registro");
+});
+signInOption.addEventListener("click", e => {
+  history.pushState(null, "Inicio", "/");
+});
+signUpBtn.addEventListener("submit", e => {
+  e.preventDefault();
+  history.pushState(null, "Bienvenida", "/bienvenida");
+});
+
+
+
+/*
 document.getElementById('nav').addEventListener('click', function(event) {
   const target = event.target;
   if (target.tagName === 'A') {
