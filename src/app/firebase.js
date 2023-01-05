5375c200bc4235aabba630d0dccee35c05694e09
Melanie Tolvett S <melanietolvett@gmail.com>
2023-01-05 11:30:53 -0300

    previo a firebase

---
+++ src/app/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBkSCFcgkxajMeLyZqGSfYJYUA-HLkav-A",
+  authDomain: "petsbook-32d4c.firebaseapp.com",
+  projectId: "petsbook-32d4c",
+  storageBucket: "petsbook-32d4c.appspot.com",
+  messagingSenderId: "166858463920",
+  appId: "1:166858463920:web:046076e36d3498c55cf975"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
