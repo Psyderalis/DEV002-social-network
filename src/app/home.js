1aaf7edd73c74417504908f750ea86e04dd66ed3
Ariel-Navarro <arielknm@gmail.com>
2023-01-09 15:27:48 -0500

    CSS de post concluido(de la rama main2cuatro

--- src/app/home.js
+++ src/app/home.js
@@ -18,9 +18,11 @@ export const homeE = (taskContainer, taskForm) => {
         <div class="cabezaDePost">
         <img class="fotoDePerfil" src="imagenes/pug.jpg" alt='foto del usuario'>
         <p class="nombreDeUsuario">Manchitas</p>
-        <ul class ="menu-horizontal"><img src="imagenes/mas.png" width=30px height=30px>
+        <ul disabled selected class ="menu-horizontal" id="mas"><img src="imagenes/mas.png" width=30px height=30px>
+         <div class="edit-delet">
          <li class='editar' data-id='${doc.id}'><img width=15px src="imagenes/editar.png"> Editar publicación</li>
          <li class='delete' data-id='${doc.id}'><img width=15px src="imagenes/eliminar.png"> Eliminar </li>
+         </div>
         </ul>
         </div>
         <div class="cuerpoDePost" >
